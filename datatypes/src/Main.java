41b2e3e1c826603d85519ac7d7bc8a5cd5e8e41c
RifatUlusoy <81752606+RifatUlusoy@users.noreply.github.com>
2023-10-16 12:40:42 +0300

    Java

---
+++ datatypes/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+	boolean isitTrue=true;
+	char letter='A';
+	int sayi=1234;
+	byte sayi2=127;
+	float sayi3=-12.12f;
+	double sayi4=14.2;
+
+    }
+}
