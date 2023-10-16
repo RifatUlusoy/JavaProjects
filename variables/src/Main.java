b0abff4c739e82b5219d1eacc1ff70921505f2be
RifatUlusoy <81752606+RifatUlusoy@users.noreply.github.com>
2023-10-16 12:34:00 +0300

    Java

---
+++ variables/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+	int OgrencıSayısı=10;
+	String mesaj="Ogrenci sayisi :";
+	System.out.println(mesaj+OgrencıSayısı);
+	
+    }
+}
